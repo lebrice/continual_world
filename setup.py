12801d7f3ba68921c01e7af0be57dea75f2aa112
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-09-02 17:46:10 -0400

    Add Versioneer
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- setup.py
+++ setup.py
@@ -7,11 +7,14 @@ packages = setuptools.find_namespace_packages(include=["continual_world*"])
 print("PACKAGES FOUND:", packages)
 print(sys.version_info)
 
+import versioneer
+
 setuptools.setup(
     name="continual_world",
-    version="0.0.1",
-    author="",
-    author_email="<TODO>",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="<author name here>",
+    author_email="<author email here>",
     description="Continual World: A Robotic Benchmark For Continual Reinforcement Learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -24,8 +27,13 @@ setuptools.setup(
     ],
     entry_points={
         "Method": [
-            # TODO: Add the methods to the package!
-            # "cndpm = cn_dpm.cndpm_method:CNDPM",
+            "sac = sequoia.methods.continualworld.base_sac_method:SAC",
+            "agem = sequoia.methods.continualworld.agem:AGEM",
+            "l2 = sequoia.methods.continualworld.reg_methods:L2RegMethod",
+            "ewc = sequoia.methods.continualworld.reg_methods:EWCRegMethod",
+            "mas = sequoia.methods.continualworld.reg_methods:MASRegMethod",
+            "packnet = sequoia.methods.continualworld.packnet:PackNet",
+            "vcl = sequoia.methods.continualworld.vcl:VCL",
         ],
     },
     python_requires='>=3.7',
