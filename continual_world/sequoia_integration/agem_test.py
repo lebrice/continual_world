864a4776f67c44109b460cce9090086d288ea645
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-09-21 14:22:37 -0400

    Fix the 'defaults' test for AGEM
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- continual_world/sequoia_integration/agem_test.py
+++ continual_world/sequoia_integration/agem_test.py
@@ -1,7 +1,11 @@
-from typing import ClassVar, Type
+from typing import ClassVar, Type, Dict, Any
 from .base_sac_method_test import TestSACMethod as SACMethodTests
 from .agem import AGEM
 
 
 class TestAGEMMethod(SACMethodTests):
     Method: ClassVar[Type[AGEM]] = AGEM
+    non_default_config_values: ClassVar[Dict[str, Any]] = {
+        "episodic_mem_per_task": 10_000,
+        "episodic_batch_size": 128,
+    }
